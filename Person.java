3eaa08cc39d82383c8db6114995cd10d5e4e0286
cuongcamauit <cuongcamauit@gmail.com>
2021-09-02 12:03:52 +0700

    edit Person.java

--- Person.java
+++ Person.java
@@ -6,11 +6,40 @@ public class Person implements Serializable {
     private int age;
     private int birthday;
     private boolean isMale;
-    public Person(String name, int name, int birthday, boolean isMale) {
+    public Person(String name, int birthday, boolean isMale) {
         this.name = name;
         this.birthday = birthday;
         this.isMale = isMale;
         this.age = YearMonth.now().getYear() - birthday;
     }
+    // setter 
+    public void setName(String name) {
+        this.name = name;
+    }
     
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+        this.age = YearMonth.now().getYear() - birthday;
+    }
+
+    public void setIsMale(boolean isMale) {
+        this.isMale = isMale;
+    }
+     
+    // getter
+    public String getName() {
+        return this.name;
+    }
+
+    public int getBirtday() {
+        return this.birthday;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public boolean getIsMale() {
+        return this.isMale;
+    }    
 } 
